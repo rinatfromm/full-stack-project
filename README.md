ed020127f6649d24c8d2a7cf638a8c282cb4b94b
Rinat Kov <frommrinat@gmail.com>
2024-03-31 15:39:44 +0200

    making comments controller

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # full-stack-project
+
+Запустите контейнер с образом MongoDB и настройками replica set (он автоматичиски скачает и запустит этот образ):
+
+надо этот:
+
+  docker run --name mongo \
+       -p 27017:27017 \
+       -e MONGO_INITDB_ROOT_USERNAME="monty" \
+       -e MONGO_INITDB_ROOT_PASSWORD="pass" \
+       -d prismagraphql/mongo-single-replica:5.0.3
+
+но так как процессор не подерживает AVX ,то мы ставим старую версию.
+
+docker run --name mongo \
+       -p 27017:27017 \
+       -e MONGO_INITDB_ROOT_USERNAME="monty" \
+       -e MONGO_INITDB_ROOT_PASSWORD="pass" \
+       -d prismagraphql/mongo-single-replica:4.4.3-bionic
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { UserController, PostController } = require("../controllers");
+const { authenticateToken } = require("../middleware/auth");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const uploadDestination = "uploads";
+
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+
+const uploads = multer({ storage: storage });
+
+//users-routes
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/current", authenticateToken, UserController.current);
+router.get("/users/:id", authenticateToken, UserController.getUserById);
+router.put("/users/:id", authenticateToken, UserController.updateUser);
+
+//posts-routes
+router.post("/posts", authenticateToken, PostController.createPost);
+router.get("/posts", authenticateToken, PostController.getAllPosts);
+router.get("/posts/:id", authenticateToken, PostController.getPostById);
+router.delete("/posts/:id", authenticateToken, PostController.deletePost);
 
 module.exports = router;
