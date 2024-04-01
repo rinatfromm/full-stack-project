7522413e2c9e06758d15e6ba819c1ffe3ca81e58
Rinat Kov <frommrinat@gmail.com>
2024-04-01 19:26:30 +0200

    fertig mit controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { UserController, PostController } = require("../controllers");
+const {
+  UserController,
+  PostController,
+  CommentController,
+  LikeController,
+  FollowController,
+} = require("../controllers");
 const { authenticateToken } = require("../middleware/auth");
 
 const uploadDestination = "uploads";
@@ -27,5 +33,21 @@ router.post("/posts", authenticateToken, PostController.createPost);
 router.get("/posts", authenticateToken, PostController.getAllPosts);
 router.get("/posts/:id", authenticateToken, PostController.getPostById);
 router.delete("/posts/:id", authenticateToken, PostController.deletePost);
+
+//comments-routes
+router.post("/comments", authenticateToken, CommentController.createComment);
+router.delete(
+  "/comments/:id",
+  authenticateToken,
+  CommentController.deleteComment
+);
+
+//like-routes
+router.post("/likes", authenticateToken, LikeController.likePost);
+router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
+
+// follow-routes
+router.post("/follow", authenticateToken, FollowController.followUser);
+router.delete("/unfollow/:id",authenticateToken, FollowController.unfollowUser);
 
 module.exports = router;
