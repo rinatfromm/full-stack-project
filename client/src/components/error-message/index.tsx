888078d6af6f16d334d226bb26167f4bd95a4be2
Rinat Kov <frommrinat@gmail.com>
2024-04-09 14:57:01 +0200

    authorization in progress

---
+++ client/src/components/error-message/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+
+const ErrorMessage = ({ error = '' }: { error: string }) => {
+
+    return error && <p className="text-red-500 mt-2 mb-5 text-small">{error}</p>;
+}
+
+export default ErrorMessage;
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,9 +1,34 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react";
+import { useState } from "react";
+import { Login } from "../../features/login";
+import { Register } from "../../features/register";
 
 
 
 
 const Auth = () => {
-    return <>Auth</>;
+    const [selected, setSelected] = useState('login')
+
+    return <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col">
+            <Card className="max-w-full w-[340px] h-[450px]">
+                <CardBody className="overflow-hidden">
+                    <Tabs
+                        fullWidth
+                        size="md"
+                        selectedKey={selected}
+                        onSelectionChange={(key) => setSelected(key as string)}>
+                        <Tab key='login' title='Вход'>
+                            <Login setSelected={setSelected} />
+                        </Tab>
+                        <Tab key='sign-up' title='Регистрация'>
+                            <Register setSelected={setSelected} />
+                        </Tab>
+                    </Tabs>
+                </CardBody>
+            </Card>
+        </div>
+    </div>;
 }
- 
+
 export default Auth;
