b9263a6a110431a2c7a6c4ebdee79a8bbbaa65cd
Rinat Kov <frommrinat@gmail.com>
2024-04-18 16:54:51 +0200

    posts vertig

--- client/src/pages/posts/index.tsx
+++ client/src/pages/posts/index.tsx
@@ -1,5 +1,46 @@
-const Posts = () => {
-    return <>Posts</>;
+
+
+
+import { useGetAllPostsQuery } from "../../app/services/postsApi"
+import { Card } from "../../components/card"
+import { CreatePost } from "../../components/create-post"
+
+export const Posts = () => {
+    const { data } = useGetAllPostsQuery()
+
+    return (
+        <>
+            <div className="mb-10 w-full flex">
+                <CreatePost />
+            </div>
+            {data && data.length > 0
+                ? data.map(
+                    ({
+                        content,
+                        author,
+                        id,
+                        authorId,
+                        comments,
+                        likes,
+                        likedByUser,
+                        createdAt,
+                    }) => (
+                        <Card
+                            key={id}
+                            avatarUrl={author.avatarUrl ?? ""}
+                            content={content}
+                            name={author.name ?? ""}
+                            likesCount={likes.length}
+                            commentsCount={comments.length}
+                            authorId={authorId}
+                            id={id}
+                            likedByUser={likedByUser}
+                            createdAt={createdAt}
+                            cardFor="post"
+                        />
+                    ),
+                )
+                : null}
+        </>
+    )
 }
- 
-export default Posts;
